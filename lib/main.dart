4fd636ce687301d3df006262af5ef82fdee68662
Leandro Freitas <leandrodavimg@Leandros-MacBook-Pro.local>
2020-04-21 13:47:34 -0300

    update project

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:contatos/pages/contact/index.dart';
+import 'package:contatos/pages/home/index.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
---
+++ lib/pages/home/index.dart
@@ -0,0 +1,186 @@
+import 'dart:io';
+
+import 'package:contatos/helpers/contact_helper.dart';
+import 'package:contatos/pages/contact/index.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  ContactHelper helper = ContactHelper();
+
+  List<Contact> contacts = List();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getAllContacts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contatos"),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showContactPage();
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(20.0),
+        itemCount: contacts.length,
+        itemBuilder: (context, index) {
+          return _contactCard(context, index);
+        },
+      ),
+    );
+  }
+
+  Widget _contactCard(BuildContext context, int index) {
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: 80.0,
+                height: 80.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: contacts[index].img != null ?
+                        FileImage(File(contacts[index].img)) :
+                        AssetImage("images/avatar.png")
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 15.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(contacts[index].name ?? "",
+                    style: TextStyle(fontSize: 22.0,
+                        fontWeight: FontWeight.bold),
+                    ),
+                    Text(contacts[index].email ?? "",
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                    Text(contacts[index].phone ?? "",
+                      style: TextStyle(fontSize: 18.0,
+                          color: Colors.grey),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      onTap: () {
+        // vai direto para a página de contatos
+        // _showContactPage(contact: contacts[index]);
+        _showOptions(context, index);
+      },
+    );
+  }
+
+  void _showOptions(BuildContext context, int index) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return BottomSheet(
+            onClosing: () {},
+            builder: (context) {
+              return Container(
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: FlatButton(
+                        child: Text("Ligar", style: TextStyle(color: Colors.red, fontSize: 20.0),),
+                        onPressed: () async {
+                          // Para ligar para o contato
+                          await launch("tel: ${contacts[index].phone}");
+                          // Para fechar a janela de options
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: FlatButton(
+                        child: Text("Editar", style: TextStyle(color: Colors.red, fontSize: 20.0),),
+                        onPressed: () {
+                          // Para fechar a janela de options
+                          Navigator.pop(context);
+
+                          // Para navegar para a tela de edição
+                          _showContactPage(contact: contacts[index]);
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: FlatButton(
+                        child: Text("Excluir", style: TextStyle(color: Colors.red, fontSize: 20.0),),
+                        onPressed: () {
+                          // Remove do banco de dados
+                          helper.deleteContact(contacts[index].id);
+                          setState(() {
+                            //Remove da lista
+                            contacts.removeAt(index);
+                            // Para fechar a janela de options
+                            Navigator.pop(context);
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        }
+    );
+  }
+
+  void _showContactPage({Contact contact}) async {
+    final recContact = await  Navigator.push(context,
+      MaterialPageRoute(builder: (context) => PageContact(contact: contact,))
+    );
+
+    if(recContact != null) {
+      if(contact != null) {
+        await helper.updateContact(recContact);
+      } else {
+        await helper.saveContact(recContact);
+      }
+      _getAllContacts();
+    }
+  }
+
+  void _getAllContacts() {
+    helper.getAllContact().then((list) {
+      setState(() {
+        contacts = list;
+      });
+    });
+  }
+}
